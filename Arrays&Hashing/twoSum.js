513225a3450794318a4d241458e97de2fadd3b85
Ivan Salgado <101019855+ivanjsalgado@users.noreply.github.com>
2023-09-11 16:37:03 -0400

    Finished twoSum

---
+++ Arrays&Hashing/twoSum.js
@@ -0,0 +1,33 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Input: (nums = [3, 2, 4]), (target = 6);
+// Output: [1, 2];
+
+// Input: (nums = [3, 3]), (target = 6);
+// Output: [0, 1];
+
+const twoSum = (nums, target) => {
+  let storeValues = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let remainder = target - nums[i];
+    if (storeValues[nums[i]] !== undefined) {
+      return [storeValues[nums[i]], i];
+    }
+    if (storeValues[remainder] === undefined) {
+      storeValues[remainder] = i;
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
